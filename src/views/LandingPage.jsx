8701f3034f812ad0d984b12f1d633e5a01573869
Anaisvenus <anais.asse-akakpo@epitech.eu>
2024-10-04 10:58:37 +0100

    fix: adding new project

---
+++ src/views/LandingPage.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+import { Link} from "react-router-dom";
+// import "./LandingPage.css";
+
+
+
+function LandingPage() {
+  return (
+    <div className="LandingPage h-screen">
+      Hello
+    </div>
+  );
+}
+
+export default LandingPage;
