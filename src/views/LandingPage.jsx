df1813a82bcd663bd05d0f177c4250b9d025c4be
Anaisvenus <anais.asse-akakpo@epitech.eu>
2024-10-08 16:38:56 +0100

    add: updating the landing page

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,10 +1,17 @@
 import Navbar from "../components/Navbar";
 import logo from "../image/logo.png";
+import { Link} from "react-router-dom";
 import devisbutton from "../image/Assets Mockup Cipa's site/Frame 4.png";
 import aspirateur from "../image/Assets Mockup Cipa's site/apirateur.png";
 import equipment from "../image/Assets Mockup Cipa's site/texte_équipements.png";
 import thumbsUp from "../image/Assets Mockup Cipa's site/thumbsUpWoman.png";
-import rectangle from "../image/Assets Mockup Cipa's site/Rectangle 12.png";
+import services1 from "../image/Assets Mockup Cipa's site/Group 24.png";
+import services2 from "../image/Assets Mockup Cipa's site/Group 25.png";
+import services3 from "../image/Assets Mockup Cipa's site/Group 26.png";
+import services4 from "../image/Assets Mockup Cipa's site/Group 27.png";
+import services5 from "../image/Assets Mockup Cipa's site/Group 28.png";
+import rectangle from "../image/Assets Mockup Cipa's site/Rectangle 7.png";
+import partners from "../image/Assets Mockup Cipa's site/partenaires CIPA 1.png";
 
 function LandingPage() {
   return (
@@ -74,6 +81,110 @@ function LandingPage() {
           doute, du vertical à l'horizontal, du propre vers le sale <br />à reculons.<br />
         </h3>
       </div>
+
+      {/* Services on the LandingPage */}
+      <div className="relative flex flex-col justify-center items-center">
+        {/* Section Title */}
+        <h1 className="text-7xl font-bold z-10 p-10 text-black  text-start">Nos Services</h1>
+
+        {/* First Row of Services */}
+        <div className="flex justify-center items-center space-x-16 mt-20"> {/* Use `space-x-16` for spacing */}
+          <div className="flex flex-col items-center"> {/* Flex column to center text under image */}
+            <Link to="/services">
+              <img 
+                className="h-40 w-auto" // Adjust height as needed
+                src={services1} 
+                alt="Service 1" 
+              />
+            </Link>
+            <Link to="/services">
+              <h1 className="mt-2 text-lg font-semibold">Entretien et Nettoyage de Bâtiment</h1> {/* Centered text */}
+            </Link>
+          </div>
+
+          <div className="flex flex-col items-center"> 
+            <Link to="/services">
+              <img 
+                className="h-40 w-auto" // Adjust height as needed
+                src={services2} 
+                alt="Service 2" 
+              />
+            </Link>
+            <Link to="/services">
+              <h1 className="mt-2 text-lg font-semibold">Entretien et nettoyage Industriel</h1> {/* Centered text */}
+            </Link>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <Link to="/services">
+              <img 
+                className="h-40 w-auto" // Adjust height as needed
+                src={services3} 
+                alt="Service 3" 
+              />
+            </Link>
+            <Link to="/services">
+              <h1 className="mt-2 text-lg font-semibold">Désherbage mécanique, chimique et Bio</h1> {/* Centered text */}
+            </Link>
+          </div>
+        </div>
+
+        {/* Second Row of Services */}
+        <div className="flex justify-center items-center space-x-16 mt-16"> {/* Use `space-x-16` for spacing */}
+          <div className="flex flex-col items-center"> 
+            <Link to="/services">
+              <img 
+                className="h-40 w-auto" // Adjust height as needed
+                src={services4} 
+                alt="Service 4" 
+              />
+            </Link>
+            <Link to="/services">
+              <h1 className="mt-2 text-lg font-semibold">Consultation et Expertise en Hygiène</h1> {/* Centered text */}
+            </Link>
+          </div>
+
+          <div className="flex flex-col items-center"> 
+            <Link to="/services">
+              <img 
+                className="h-40 w-auto" // Adjust height as needed
+                src={services5} 
+                alt="Service 5" 
+              />
+            </Link>
+            <Link to="/services">
+              <h1 className="mt-2 text-lg font-semibold">Ventes de matériels et produits de désinfection</h1> {/* Centered text */}
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      {/* Partners on the LandingPage */}
+      <div className="relative mt-14">
+        {/* Image de fond */}
+        <img 
+          className="w-full h-auto transform -translate-y-10 z-0 opacity-50 bg-grey-200" 
+          src={rectangle} 
+          alt="Thumbs Up Woman" 
+        />
+        <div className="absolute inset-0 transform -translate-y-10 z-10 bg-zinc-900" />
+
+        <h1 className="absolute top-0 right-40 text-7xl font-bold z-10 p-10 text-white">Nos Références</h1>
+        {/* Texte superposé à l'image */}
+        <img className="absolute top-28 left-10 z-10" style={{ height: '700px', width: '1400px' }} src={partners} alt="Logo" />
+
+
+      </div>
+
+
+      {/* </div> */}
+
+
+
+      {/* Partners on the LandingPage */}
+      {/* <div>
+        <h1>Nos Références</h1>
+      </div> */}
 
     </div>
   );
